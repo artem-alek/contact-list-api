c7a1594060721ac9ce18a32de7e1cb10fe199169
Artem Aleksandruk <artem.alek@icloud.com>
2017-03-14 15:02:45 -0400

    changed app.use

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,16 @@
 // Require your controllers here
 const ContactController = require("../controllers/contact");
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 module.exports = (app) => {
   // Add your routes here
+
+  app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+  });
+    
   app.post('/contacts', ContactController.create);
   app.get('/contacts/list', ContactController.listContacts);
   app.get('/contact/:id', ContactController.getContact);
