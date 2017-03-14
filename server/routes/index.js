44d557dd85a79ac3862e71a2d31422cf9d1c24a0
Artem Aleksandruk <artem.alek@icloud.com>
2017-03-14 14:42:32 -0400

    added cors

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,11 @@
 // Require your controllers here
 const ContactController = require("../controllers/contact");
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 module.exports = (app) => {
   // Add your routes here
